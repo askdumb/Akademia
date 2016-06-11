551c76e74002300091726762af3795cf26e16088
askdumb <askdumb@wp.pl>
2016-06-11 18:48:53 +0200

    Snake Game

--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -1,28 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Snake
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
+            Settings.snakeSize = (int)size.big;
+            Settings.foodSize = (int)size.big;
+            Settings.timerSpeed = (int)timeSpan.moderate;
             InitializeComponent();
+            Switcher.mainWindow = this;
+            Switcher.Switch(new MainMenu());
         }
+
+
+        public void Navigate(UserControl anotherPage)
+        {
+            this.Content = anotherPage;
+        }
+
+
+        public void Navigate(UserControl anotherPage, object sender)
+        {
+            this.Content = anotherPage;
+            ISwitchable s = anotherPage as ISwitchable;
+
+            if (s != null)
+                s.UtilizeState(sender);
+            else
+                throw new ArgumentException("NextPage is not ISwitchable! "
+                  + anotherPage.Name.ToString());
+        }
+
+
+        public void KeyPressed(object sender, KeyEventArgs e)
+        {
+            this.Focus();
+            Input.StateOfKey(e.Key, true);
+        }
+
+
+        public void KeyReleased(object sender, KeyEventArgs e)
+        {
+            this.Focus();
+            Input.StateOfKey(e.Key, false);
+        }
+
     }
 }
